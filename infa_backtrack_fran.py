e5c14f78c1f10c841b9aa2cab37394b2b307d523
Fran Majdak <majdak725@gmail.com>
2018-06-07 19:30:38 +0200

    fixed grammar
    60

--- infa_backtrack_fran.py
+++ infa_backtrack_fran.py
@@ -1,10 +1,10 @@
 """ova verzija programa radi tako da pokusava zavrsiti
-tablicu pokusavanjem jednog tip pomaka i kada ne uspije
+tablicu pokusavanjem jednog tipa pomaka i kada ne uspije
 vrati se za jedan korak natrag i proba drugi tip pomaka
 a.k.a. Backtracking"""
 
 import random
-print("rjesenje problema za \"knight\'s tour\"")
+print("Rjesenje problema za \"knight\'s tour\"")
 
 # liste za sva polja i posjecena polja
 polja = []
@@ -17,7 +17,6 @@ for i in range(1, 82):
 p = random.choice(polja)  # odabire random polje s kojeg krece
 posjecena_polja.append(p)  # stavlja pocetno polje u listu posjecena_polja
 polja.remove(p)				# brise pocetno polje iz liste polja
-# odabire random polje s kojeg krece
 
 # tipovi pomaka:
 a = p + 17  # dolje_lijevo_dugo
@@ -27,4 +26,4 @@ d = p + 11  # dolje_desno_kratko
 e = p - 19  # gore_lijevo_dugo
 f = p - 11  # gore_lijevo_kratko
 g = p - 17  # gore_desno_dugo
-h = p - 7  # gore_desno_katko
+h = p - 7  # gore_desno_kratko
